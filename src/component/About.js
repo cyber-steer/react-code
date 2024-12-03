5d27dc666903a1354ef23fcb91f40a1616bb182c
cyber-steer <cybersteer@naver.com>
2024-12-03 18:00:55 +0900

    commit

--- src/component/About.js
+++ src/component/About.js
@@ -4,33 +4,12 @@ import { Container, Row, Button, Col, Badge } from 'react-bootstrap';
 const questions = {
   UNIX시스템: [
     {
-      question: "다음중 GNU 프로젝트와의 관련성이 가장 적은 것은?",
-      options: ["리처드 스톨먼", "GPL", "Apache 서버", "자유 소프트웨어 운동"],
-      answer: 0
-    },
-    {
-      question: "리눅스 표준 디렉터리의 용도에 관한 설명으로 잘못된 것은?",
-      options: ["/proc - 커널이 사용하는 가상의 파일 시스템",
-                "/home - 사용자의 홈 디렉터리가 생성되는 곳",
-                "/dev - 시스템 운영 중에 생기는 가변 자료가 저장되는 곳",
-                "/etc - 시스템의 중요 환경 설정 파일이 위치하는 곳"],
-      answer: 0
-    },
-    {
-      question: "셸의 명령 행에서 사용하는 특수 문자에 관한 설명이다. 잘못된 것은?",
-      options: ["| : 특수 문자 앞에 붙어 특수 문자의 기능을 제거함",
-                ">> : 표준 출력을 파일의 끝에 덧붙일 때 사용함",
-                "# : 이후의 문자들이 주석으로 처리됨",
-                "! : 명령 히스토리 기능을 이용할 때 사용함"],
-      answer: 0
-    },
-    {
-      question: "다음 명령의 실행 결과로 생각되는 것은? 맨 앞의 $는 셸 프롬프트이며 passwd는 비밀번호를 바꿀 때 사용하는 명령이다.",
-      content: 'content',
-      options: ["",
-                "",
-                "",
-                ""],
+      question: "다음은 셸 스크립트의 일부이다. 밑줄 그은 부분의 의미를 바르게 설명한것은?",
+      content: '# .bashrc\nif <u>[ -f /etc/bashrc ]</u>; then\n        . /etc/bashrc\nfi',
+      options: ["/etc/bashrc가 존재하고 정규 파일이면 참",
+                "/etc/bashrc가 존재하고 디렉터리이면 참",
+                "/etc/bashrc가 실행 가능한 파일이면 참",
+                "/etc/bashrc가 존재하지 않으면 참"],
       answer: 0
     },
   ],
@@ -137,6 +116,16 @@ function About() {
     backgroundColor: '#f9f9f9',
     marginBottom: '20px'
   };
+  
+  const contentStyle = {
+    border: '1px solid #ddd',
+    backgroundColor: '#f9f9f9',
+  };
+  
+  const contentPreStyle = {
+    margin: '10px',
+    textAlign: 'left'
+  };
 
   const formGroupStyle = {
     display: 'flex',
@@ -192,11 +181,10 @@ function About() {
         <>
           <div style={questionStyle}>
             <p>{questions[selectedSubject][currentQuestionIndex].question}</p>
-            {questions[selectedSubject][currentQuestionIndex].content && ( <pre>{questions[selectedSubject][currentQuestionIndex].content}</pre> )}
           </div>
           {questions[selectedSubject][currentQuestionIndex].content && (
-            <div style={questionStyle}>
-                <pre>{questions[selectedSubject][currentQuestionIndex].content}</pre>
+            <div style={contentStyle}>
+                <pre style={contentPreStyle}>{questions[selectedSubject][currentQuestionIndex].content}</pre>
             </div>
           )}
           <div>
